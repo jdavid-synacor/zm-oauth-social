d6d9d9209acaa1f37214272b1a871935ac68dfa8
Jonathan David <jdavid@synacor.com>
2018-06-06 09:47:59 -0400

    ZCS-5097: Create Facebook contact importer for zm-oauth-social

--- src/java/com/zimbra/oauth/handlers/impl/FacebookOAuth2Handler.java
+++ src/java/com/zimbra/oauth/handlers/impl/FacebookOAuth2Handler.java
@@ -26,8 +26,10 @@ import org.apache.commons.lang.StringUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.zimbra.client.ZMailbox;
+import com.zimbra.client.ZFolder.View;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.filter.jsieve.ZimbraVariablesCtrl;
 import com.zimbra.oauth.handlers.IOAuth2Handler;
 import com.zimbra.oauth.models.OAuthInfo;
 import com.zimbra.oauth.utilities.Configuration;
@@ -161,6 +163,11 @@ public class FacebookOAuth2Handler extends OAuth2Handler implements IOAuth2Handl
          */
         public static final String HOST_FACEBOOK = "graph.facebook.com";
 
+        /**
+         * The contacts uri template.
+         */
+        public static final String CONTACTS_URI_TEMPLATE = "https://graph.facebook.com/v3.0/me/friends?access_token=%s&fields=%s";
+
     }
 
     /**
@@ -178,6 +185,9 @@ public class FacebookOAuth2Handler extends OAuth2Handler implements IOAuth2Handl
         authenticateUri = FacebookConstants.AUTHENTICATE_URI;
         authorizeUri = buildAuthorizeUri(FacebookConstants.AUTHORIZE_URI_TEMPLATE);
         relayKey = FacebookConstants.RELAY_KEY;
+        // add associated import classes
+        dataSource.addImportClass(View.contact.name(),
+            FacebookContactsImport.class.getCanonicalName());
     }
 
     /**
@@ -207,6 +217,7 @@ public class FacebookOAuth2Handler extends OAuth2Handler implements IOAuth2Handl
         oauthInfo.setUsername(username);
         oauthInfo.setRefreshToken(credentials.get("access_token").asText());
         dataSource.syncDatasource(mailbox, oauthInfo);
+        ZimbraLog.extensions.info("******* ACCESS TOKEN ******** " + credentials.get("access_token").asText());
         return true;
     }
 
